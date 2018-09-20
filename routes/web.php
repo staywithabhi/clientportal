ba2491e9ff43929d3bdeb73222bdeda0d025349c
Abhishek dixit <abhishek@vortexglobal.in>
2018-09-20 18:33:08 +0530

    authentication added

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::auth();
+
+
+Route::get('/', function () {
+    return view('home');
+    })->middleware('auth');
+    
 Route::group(['middleware' => 'auth'], function () {
     //    Route::get('/link1', function ()    {
 //        // Uses Auth Middleware
