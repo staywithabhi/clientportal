9d04159ffe63b088a6c39f3d45fbcc14e245a489
Abhishek dixit <abhishek@vortexglobal.in>
2018-09-27 18:33:43 +0530

    remote connection added

--- routes/web.php
+++ routes/web.php
@@ -11,25 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::auth();
 
 
 Route::get('/', function () {
     return view('home');
     })->middleware('auth');
-    
-Route::group(['middleware' => 'auth'], function () {
-    //    Route::get('/link1', function ()    {
-//        // Uses Auth Middleware
-//    });
-
-    //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
-    #adminlte_routes
-});
 Route::get('profile','UserController@profile');
 Route::post('profile','UserController@update_profile');
 // Route::post('uploadImage','ClientController@imageUpload');
