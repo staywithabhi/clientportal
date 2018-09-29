c75e208be74afa34805e3d5e7c9b59ec75804e0c
Abhishek dixit <abhishek@vortexglobal.in>
2018-09-29 18:32:47 +0530

    project done

--- resources/views/vendor/adminlte/home.blade.php
+++ resources/views/vendor/adminlte/home.blade.php
@@ -41,7 +41,7 @@
                                 </div>
                             </div>
                         </div>
-                      <a href="#">
+                      <a href="{{ route('manageMembers') }}">
                             <div class="panel-footer">
                                 <span class="pull-left">View Details</span>
                                 <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
